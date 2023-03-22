f95219df2e0d71b1892c71ceba89d88d270e7f38
Bereke Sakan <bereke3103@mail.ru>
2023-03-22 10:47:01 +0600

    CreateModelCategory

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string DisplayOrder { get; set; }
+
+        public DateTime CreateedDateTime { get; set; } = DateTime.Now;
+    }
+}

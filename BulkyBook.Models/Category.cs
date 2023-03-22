94ace69a5d614b01e6377882b6d013d907928c16
Bereke Sakan <bereke3103@mail.ru>
2023-03-23 05:51:41 +0600

    MVCExtends

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+
+        //Data Annotations
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        //Display Name Range Validations
+        [DisplayName("Display Order Range(Max-150)")]
+        [Range(1, 150, ErrorMessage = "Display Order must be between 1 and 100 only!!!")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateedDateTime { get; set; } = DateTime.Now;
+    }
+}

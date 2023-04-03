ff29df539e04fb8a31fd3bc0a78c4f49837aae54
Bereke Sakan <bereke3103@mail.ru>
2023-04-03 18:36:53 +0600

    email sendler

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,20 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailsToSend = new MimeMessage();
+            emailsToSend.From.Add(MailboxAddress.Parse("berekesakan@yandex.ru"));
+            emailsToSend.To.Add(MailboxAddress.Parse(email));
+            emailsToSend.Subject = subject;
+            emailsToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage };
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.yandex.ru", 465, true);
+                emailClient.Authenticate("berekesakan@yandex.ru", "Zt5RXSM39jJF.Zk");
+                emailClient.Send(emailsToSend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
